f93434280c6d415255fc2a327b7cfa058112a73e
Shreerang B <shreerangb@ideyalabs.com>
2026-02-04 23:50:40 +0530

    Create yedhd.py

---
+++ yedhd.py
@@ -0,0 +1,8 @@
+def two_sum(nums, target):
+    seen = {}
+    for i, n in enumerate(nums):
+        if target - n in seen:
+            return [seen[target - n], i]
+        seen[n]  i
+
+print(two_sum([2, 7, 11, 15] 9))
