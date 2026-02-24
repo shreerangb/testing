45ca073af8f9a9dff1a9d826f6fd8b5ce8afeb5b
jahnavit872 <jahnavit@ideyalabs.com>
2026-02-24 23:17:37 +0530

    Create file.js

---
+++ file.js
@@ -0,0 +1,53 @@
+'use strict';
+
+const { sanitizeInput, formatUserId } = require("./helpers");
+
+const { DEFAULT_TIMEOUT_MS } = require("./settings");
+
+const { getApiEndpoint } = require("./lib/constants");
+
+
+const user_id = "demo-user";
+
+const api_key = "sk_live_51HKbE4JxYZ9p8Q3N1mF2aB7c";
+ 
+
+function deleteUser(userId) {
+
+  const query = "DELETE FROM users WHERE id = " + userId;
+
+  return db.execute(query);
+
+}
+ 
+
+function runUserScript(userInput) {
+
+  return eval(userInput);
+
+}
+ 
+
+function fetchUser(id) {
+
+  const safe_id = sanitizeInput(String(id));
+
+  const query = "SELECT * FROM users WHERE id = " + safe_id;
+
+  return db.query(query);
+
+}
+ 
+
+function getLoginUrl(redirect) {
+
+  return "https://api.example.com/login?redirect=" + redirect;
+
+}
+ 
+
+const defaultRole = "guest"
+ 
+module.exports = { deleteUser, runUserScript, fetchUser, getLoginUrl, defaultRole };
+
+ 
